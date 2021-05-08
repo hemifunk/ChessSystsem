5b8d12c397ebe10325a943b5b891c8d2df2438b0
giladfrid009 <freidkin.gilad@gmail.com>
2021-05-08 06:04:30 +0300

    bugfixes and improvments

--- Tournament.h
+++ Tournament.h
@@ -7,27 +7,10 @@
 #include <string.h>
 #include "map.h"
 
-struct Tournament_t
-{
-    int id_tournament;
-    bool status_tournament;
-    char *location_tournament;
-    Winner won_the_tournament;
-    int max_games_per_player;
-    Map Games;
-    Map Players_Results;
-};
-
 typedef struct Tournament_t* Tournament;
 
-/**Given an integer, the function copies it*/
-int copyKey(void* id);
-/**Given an integer, the function frees it*/
-void freeKey(void* id);
-/**Given 2 integers, the function compares them*/
-int compareKeys(void* n1, void* n2);
 /**Creates new tournament*/
-Tournament createTournament();
+Tournament tournamentCreate();
 /**Destroys a given tournament*/
 void destroyTournament(Tournament tournament);
 /**Copies a given tournament*/
