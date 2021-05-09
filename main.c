da6429600506679569c9384565007ab70410781f
giladfrid009 <freidkin.gilad@gmail.com>
2021-05-09 20:48:42 +0300

    added small test logic to main

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
+#include "chess.h"
+
 int main()
 {
+	ChessSystem chess = chessCreate();
+
+	ChessResult result = CHESS_SUCCESS;
+
+	result = chessAddGame(chess, 1, 11, 12, DRAW, 60);
+
+	result = chessAddTournament(chess, 1, 10, "Israel");
+
+	result = chessAddGame(chess, 1, 11, 12, DRAW, 60);
+
+	result = chessAddGame(chess, 1, 12, 11, DRAW, 60);
+
+	result = chessAddGame(chess, 2, 11, 12, DRAW, 60);
+
+	chessDestroy(chess);
+
+	printf("result: %d", result);
+
 	return 0;
 }
